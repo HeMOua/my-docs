4d57980e738558ca7e1dc3e3c8a4965b10e14a68
HeMOu <fangqichenchao@163.com>
2025-03-05 22:10:54 +0800

    feat: 添加网站元数据信息以及站长统计

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -9,6 +9,33 @@ export default defineUserConfig({
   title: "技数斋",
   description: "前后端 + 人工智能",
 
+  head: [
+    ["meta", { name: "robots", content: "all" }],
+    ["meta", { name: "author", content: "贺墨于" }],
+    ["meta", { name: "author", content: "HeMOu" }],
+    ["meta", { name: "author", content: "HeMOua" }],
+    [
+      "meta",
+      {
+        name: "keywords",
+        content:
+          "Java, 前端, 后端, 人工智能, 深度学习, 神经网络, yolo, 大模型, LLM"
+      }
+    ],
+    ["meta", { name: "apple-mobile-web-app-capable", content: "yes" }],
+    [
+      "script",
+      {},
+      `var _hmt = _hmt || [];
+        (function() {
+          var hm = document.createElement("script");
+          hm.src = "https://hm.baidu.com/hm.js?1f34b9cd7d3a338d3e974a5ca76a99df";
+          var s = document.getElementsByTagName("script")[0]; 
+          s.parentNode.insertBefore(hm, s);
+        })();`
+    ]
+  ],
+
   theme
 
   // 和 PWA 一起启用
